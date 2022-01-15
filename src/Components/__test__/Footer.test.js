c178c75802f0f97190b55cd02dc0d2c91be0b47f
eugenehan94 <eugene.han94@gmail.com>
2022-01-14 23:10:51 -0500

    added more testing

--- src/Components/__test__/Footer.test.js
+++ src/Components/__test__/Footer.test.js
@@ -2,9 +2,6 @@ import React from "react";
 import { render, screen } from "./test-utils";
 import Footer from "../Footer";
 
-test("Footer text present", () => {
+it("Footer renders", () => {
   render(<Footer />);
-  screen.debug();
-  // const text = screen.getByRole("heading", { name: /hello world/i });
-  // expect(text).toBeInTheDocument;
 });
--- src/Components/__test__/Navbar.test.js
+++ src/Components/__test__/Navbar.test.js
@@ -1,8 +1,24 @@
 import React from "react";
-import {render, screen} from "./test-utils";
-import Navbar from "../Navbar"
+import { render, screen } from "./test-utils";
+import Navbar from "../Navbar";
+import { MemoryRouter } from "react-router-dom";
 
-test("Navbar", () => {
-    render(<Navbar/>) 
-    screen.debug();
-})
+// const renderWithRouter = (ui, { route = "/" } = {}) => {
+//   window.history.pushState({}, "Test page", route);
+//   return render(ui, { wrapper: BrowserRouter });
+// };
+
+describe("Navbar links present", () => {
+  it("Home button AniMan is present", () => {
+    render(<Navbar />, { wrapper: MemoryRouter });
+    expect(screen.getByText(/Animan/i)).toBeInTheDocument();
+  });
+  it("Manga button is present", () => {
+      render(<Navbar/>, {wrapper: MemoryRouter});
+      expect(screen.getByText(/manga/i)).toBeInTheDocument();
+  })
+  it("Search button is present", () => {
+      render(<Navbar/>, {wrapper: MemoryRouter});
+      expect(screen.getByText(/search/i)).toBeInTheDocument();
+  })
+});
