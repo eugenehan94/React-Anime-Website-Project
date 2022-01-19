1dcb835d930e64ca4f1274ffa513b0bb0af68e74
eugenehan94 <eugene.han94@gmail.com>
2022-01-18 20:47:01 -0500

    added test to mangaReducer

--- src/Redux/Reducers/__test__/mangaReducer.test.js
+++ src/Redux/Reducers/__test__/mangaReducer.test.js
@@ -1,6 +1,6 @@
 import {mangaReducer} from "../mangaReducer"
 
-import {selectedDropdown} from "../../Actions/mangaActions"
+import { selectedDropdown, selectedList } from "../../Actions/mangaActions";
 
 it("return initial states of mangaReducer", () => {
     expect(mangaReducer(undefined, {})).toEqual({
@@ -8,4 +8,30 @@ it("return initial states of mangaReducer", () => {
       mangaList: null,
       loading: true,
     });
+})
+
+it("it should handle the selected dropdown option", () => {
+  const previousState = {
+    selectedChoice: "manga",
+    mangaList: null,
+    loading: true,
+  };
+  expect(mangaReducer(previousState, selectedDropdown("anime"))).toEqual({
+    selectedChoice: "anime",
+    mangaList: null,
+    loading: true,
+  });
+})
+
+it("it should handle the list of manga", () => {
+  const previousState = {
+    selectedChoice: "manga",
+    mangaList: null,
+    loading: true,
+  };
+  expect(mangaReducer(previousState, selectedList("listOfAnime"))).toEqual({
+    selectedChoice: "manga",
+    mangaList: "listOfAnime",
+    loading: false,
+  });
 })
