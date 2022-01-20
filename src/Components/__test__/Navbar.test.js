b0b65a0b2494fbe6a830e4557cf531e8a6844089
eugenehan94 <eugene.han94@gmail.com>
2022-01-19 19:10:11 -0500

    working on component testing

--- src/Components/__test__/Navbar.test.js
+++ src/Components/__test__/Navbar.test.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { render, screen } from "./test-utils";
+import userEvent from "@testing-library/user-event";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
 import Navbar from "../Navbar";
 import { MemoryRouter } from "react-router-dom";
 
-// const renderWithRouter = (ui, { route = "/" } = {}) => {
-//   window.history.pushState({}, "Test page", route);
-//   return render(ui, { wrapper: BrowserRouter });
-// };
+const renderWithRouter = (ui, { route = "/" } = {}) => {
+  window.history.pushState({}, "Test page", route);
+  return render(ui, { wrapper: BrowserRouter });
+};
 
 describe("Navbar links present", () => {
   it("Home button AniMan is present", () => {
@@ -21,4 +24,14 @@ describe("Navbar links present", () => {
       render(<Navbar/>, {wrapper: MemoryRouter});
       expect(screen.getByText(/search/i)).toBeInTheDocument();
   })
+});
+
+test("full app rendering/navigating", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <Navbar/>
+    </Router>
+  );
+
 });
