2cb27a9a3b41f391a68a0e918b21cd386c546f50
eugenehan94 <eugene.han94@gmail.com>
2023-01-25 19:43:53 -0500

    removed un-used comments

--- src/Components/RandomHero.js
+++ src/Components/RandomHero.js
@@ -8,7 +8,6 @@ import {
   Typography,
 } from "@mui/material";
 import { useSelector } from "react-redux";
-import demonSlayerHeroImage from "../Images/demonSlayerHeroImage.jpg";
 
 const RandomHero = () => {
   const data = useSelector((state) => state);
@@ -130,40 +129,6 @@ const RandomHero = () => {
         <Grid item xs={3} />
       </Grid>
     </Box>
-    // <Box sx={{ position: "relative", height: "350px", width: "400px"}}>
-    /* <img
-        src={images.jpg.image_url}
-        alt=""
-        style={{ width: "100%", height: "100%", objectFit: "fill" }}
-      />
-      <Box
-        sx={{
-          position: "absolute",
-          backgroundColor: "#2a2c37",
-          color: "white",
-          top: 0,
-          left: 0,
-          padding: "0.5rem",
-        }}
-      >
-        {title}
-      </Box>
-      {genres.length > 0 ? (
-        <Box
-          sx={{
-            position: "absolute",
-            backgroundColor: "#2a2c37",
-            color: "white",
-            bottom: 0,
-            left: 0,
-            width: "100%",
-            padding: "0.5rem",
-          }}
-        >
-          {genres.map((genre) => genre.name + " ")}
-        </Box>
-      ) : null} */
-    // </Box>
   );
 };
 
